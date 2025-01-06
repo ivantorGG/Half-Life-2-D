5958a679fcc2e35ee9a7ee9ab87a2bea4e79f496
Ivan <Ivan2009e@yandex.ru>
2025-01-06 20:19:55 +0300

    now main file is normal

--- __main__.py
+++ __main__.py
@@ -1,43 +1,21 @@
-# меня преследует эта проблема с поворотами ☠️
 import pygame
 
 
 pygame.init()
-size = width, height = 201, 201
+size = width, height = 1500, 800
 
 screen = pygame.display.set_mode(size)
 
-screen2 = pygame.Surface(screen.get_size())
-pygame.draw.circle(screen2, (255, 255, 255), (100, 100), 10)
-pygame.draw.polygon(screen2, (255, 255, 255), ((100, 100), (82, 30), (118, 30)))
-pygame.draw.polygon(screen2, (255, 255, 255), ((100, 100), (171, 121), (150, 150)))
-pygame.draw.polygon(screen2, (255, 255, 255), ((100, 100), (31, 121), (52, 150)))
-screen.blit(screen2, (0, 0))
-
-FPS = 100
+FPS = 60
 running = True
-angle = 0
-plus_angle = 0
 cloak = pygame.time.Clock()
-ROTATEVENTEVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(ROTATEVENTEVENT, 50)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                plus_angle += 5
-            if event.button == 3:
-                plus_angle -= 5
-        if event.type == ROTATEVENTEVENT:
-            angle += plus_angle
-            angle %= 360
-            screen.blit(pygame.transform.rotate(screen2, angle), (0, 0))
 
     pygame.display.flip()
     cloak.tick(FPS)
-
 
 pygame.quit()
